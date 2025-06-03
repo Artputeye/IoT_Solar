20f41b45ec2891f721a5cad7c9e38efc092c8cfd
Artputeye <ongat.kh@gmail.com>
2025-06-03 19:43:20 +0700

    add html client request to server

--- src/serv.cpp
+++ src/serv.cpp
@@ -1,39 +1,47 @@
 #include "globals.h"
-
 #include "serv.h"
-// #include <AsyncTCP.h>
-// #include <ESPAsyncWebServer.h>
-// AsyncWebServer server(80);
+const char *PARAM_MESSAGE PROGMEM = "plain";
 
-void handlePostData(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
-  String body = "";
-  for (size_t i = 0; i < len; i++) {
-    body += (char)data[i];
-  }
-  Serial.println("POST client: " + body);
-  request->send(200, "text/plain", "POST received: " + body);
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-String getContentType(String filename) {
-  if (filename.endsWith(".html")) return "text/html";
-  if (filename.endsWith(".css")) return "text/css";
-  if (filename.endsWith(".js")) return "application/javascript";
-  if (filename.endsWith(".png")) return "image/png";
-  if (filename.endsWith(".jpg")) return "image/jpeg";
-  if (filename.endsWith(".ico")) return "image/x-icon";
-  if (filename.endsWith(".svg")) return "image/svg+xml";
-  if (filename.endsWith(".json")) return "application/json";
+String getContentType(String filename)
+{
+  if (filename.endsWith(".html"))
+    return "text/html";
+  if (filename.endsWith(".css"))
+    return "text/css";
+  if (filename.endsWith(".js"))
+    return "application/javascript";
+  if (filename.endsWith(".png"))
+    return "image/png";
+  if (filename.endsWith(".jpg"))
+    return "image/jpeg";
+  if (filename.endsWith(".ico"))
+    return "image/x-icon";
+  if (filename.endsWith(".svg"))
+    return "image/svg+xml";
+  if (filename.endsWith(".json"))
+    return "application/json";
   return "text/plain";
 }
 
-void setupServer() {
-  // POST endpoint
-  server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){}, NULL, handlePostData);
+void setupServer()
+{
+
+  server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request)
+            {
+    String message;
+    if (request->hasParam(PARAM_MESSAGE, true)) {
+      message = request->getParam(PARAM_MESSAGE, true)->value();
+    } else {
+      message = "No message sent";
+      Serial.println("No message sent");
+    }
+    dataToinv(message);
+    Serial.println("POST client: " + message);
+    request->send(200, "text/plain", "POST: " + message); });
 
   // เสิร์ฟไฟล์ static จาก LittleFS
-  server.onNotFound([](AsyncWebServerRequest *request) {
+  server.onNotFound([](AsyncWebServerRequest *request)
+                    {
     String path = request->url();
     if (path == "/") path = "/index.html";
     String contentType = getContentType(path);
@@ -42,8 +50,99 @@ void setupServer() {
       request->send(LittleFS, path, contentType);
     } else {
       request->send(404, "text/plain", "File Not Found");
-    }
-  });
+    } });
 
   server.begin();
+}
+
+void dataToinv(String data)
+{
+  if (data == "Grid-tie operation ON")
+  {
+    inv.cmd_inv("grid on");
+  }
+  if (data == "Grid-tie operation OFF")
+  {
+    inv.cmd_inv("grid off");
+  }
+  if (data == "Beeps While Primary Source Interrupt ON")
+  {
+    inv.cmd_inv("beeps on");
+  }
+  if (data == "Beeps While Primary Source Interrupt OFF")
+  {
+    inv.cmd_inv("beeps off");
+  }
+  if (data == "LCD Backlight ON")
+  {
+    inv.cmd_inv("blacklight on");
+  }
+  if (data == "LCD Backlight OFF")
+  {
+    inv.cmd_inv("blacklight off");
+  }
+  if (data == "Return To The Main LCD Page ON")
+  {
+    inv.cmd_inv("returnpage on");
+  }
+  if (data == "Return To The Main LCD Page OFF")
+  {
+    inv.cmd_inv("returnpage off");
+  }
+  if (data == "Power Saving Mode ON")
+  {
+    inv.cmd_inv("saving on");
+  }
+  if (data == "Power Saving Mode OFF")
+  {
+    inv.cmd_inv("saving off");
+  }
+  if (data == "Over Temp Auto Restart ON")
+  {
+    inv.cmd_inv("overtemp on");
+  }
+  if (data == "Over Temp Auto Restart OFF")
+  {
+    inv.cmd_inv("overtemp off");
+  }
+  if (data == "Led Pattern Light ON")
+  {
+    inv.cmd_inv("ledpattern on");
+  }
+  if (data == "Led Pattern Light OFF")
+  {
+    inv.cmd_inv("ledpattern off");
+  }
+  if (data == "Buzzer ON")
+  {
+    inv.cmd_inv("buzzer on");
+  }
+  if (data == "Buzzer OFF")
+  {
+    inv.cmd_inv("buzzer off");
+  }
+  if (data == "Dual Output ON")
+  {
+    inv.cmd_inv("dualoutput on");
+  }
+  if (data == "Dual Output OFF")
+  {
+    inv.cmd_inv("dualoutput off");
+  }
+  if (data == "Overload Auto Restart ON")
+  {
+    inv.cmd_inv("dualoutput on");
+  }
+  if (data == "Overload Auto Restart OFF")
+  {
+    inv.cmd_inv("dualoutput off");
+  }
+  if (data == "Transfer To Bypass Overload ON")
+  {
+    inv.cmd_inv("dualoutput on");
+  }
+  if (data == "Transfer To Bypass Overload OFF")
+  {
+    inv.cmd_inv("dualoutput off");
+  }
 }
--- src/serv.h
+++ src/serv.h
@@ -4,5 +4,5 @@
 #include "globals.h"
 
 void setupServer();
-
+void dataToinv(String data);
 #endif
